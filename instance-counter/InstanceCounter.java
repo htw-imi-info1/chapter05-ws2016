2c250cbac50e44d1540d42b88a3506d68c572029
B Kleinen <kleinen@infrastructure.de>
2013-12-16 15:57:02 +0100

    empty class prepared

---
+++ instance-counter/InstanceCounter.java
@@ -0,0 +1,15 @@
+
+/**
+ * This class counts its instances.
+ * 
+ * @author Barbara Kleinen 
+ * @version 16.12.2013
+ */
+public class InstanceCounter
+{
+
+    public InstanceCounter()
+    {
+    }
+    
+}
