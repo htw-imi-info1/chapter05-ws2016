56010576baa2899fdb034eddeeb123c1f19374e6
B Kleinen <kleinen@infrastructure.de>
2016-12-01 16:02:57 +0100

    in class 1/12/16

--- instance-counter/InstanceCounter.java
+++ instance-counter/InstanceCounter.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 /**
  * This class counts its instances.
  * 
@@ -6,9 +6,40 @@
  */
 public class InstanceCounter
 {
+    
+    Random rand = new Random();
+    public void x(){
+        Random rand = new java.util.Random();
+        int i = new rand.nextInt(6);
+    }
+    
+    
+    
+    public static final double PI = 3.14;
+    static int instanceCounter = 0;
+    int instanceNumber = 0;
 
     public InstanceCounter()
     {
+        instanceCounter++;
+        instanceNumber = instanceCounter;
     }
-    
+
+    public int getInstanceNumber(){
+        return instanceNumber;
+    }
+
+    public void test() 
+    {
+        int sum = 1;
+
+        for (int i = 0; i <= 4; i++)
+        {
+            sum = sum + 1;
+        }
+
+        System.out.println("The result is: " + sum);
+        System.out.println("Double result: " + sum+sum);
+    }
+
 }
